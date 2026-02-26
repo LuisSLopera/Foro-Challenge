8b8d67943a4e63ae002d85d646a0a102f5170147
Luis Lopera <sistemas@zencillo.com>
2026-02-25 21:11:21 -0500

    Re ajuste de estructura de carpetas para flyway

--- src/main/resources/db/migration/V2__create-table-publicaciones.sql
+++ src/main/resources/db/migration/V2__create-table-publicaciones.sql
@@ -7,10 +7,4 @@ CREATE TABLE IF NOT EXISTS publicaciones (
   estado TINYINT NOT NULL,
   usuario_id BIGINT NOT NULL,
   curso VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id),
-  INDEX usuario_id_idx (usuario_id ASC) VISIBLE,
-  CONSTRAINT usuario_id
-    FOREIGN KEY (usuario_id)
-    REFERENCES mydb.usuarios (idusuario)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  PRIMARY KEY (id));
